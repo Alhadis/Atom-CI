6f3a5beba927ff7127c5543a8edf93ecc164d952
Alhadis <gardnerjohng@gmail.com>
2019-07-07 01:08:00 +1000

    Refactor functions for querying GitHub's API

--- dist/main.sh
+++ dist/main.sh
@@ -3,13 +3,20 @@ set -e
 
 # Print a colourful "==> $1"
 title(){
-	printf >&2 '\e[32m==>\e[0m \e[1m%s\e[0m\n' "$1"
+	printf >&2 '\e[34m==>\e[0m \e[1m%s\e[0m\n' "$1"
 }
 
 # Print a command before executing it
 cmd(){
-	printf >&2 '\e[38;5;28m$\e[0m \e[32m%s\e[0m\n' "$*"
+	printf >&2 '\e[32m$ %s\e[0m\n' "$*"
 	"$@"
+}
+
+# Terminate execution with an error message
+die(){
+	printf >&2 '\e[1;31mfatal:\e[22m %s\e[0m\n' "$1"
+	shift
+	exit ${1:-1}
 }
 
 # Check if a devDependency is listed in package.json
@@ -22,109 +29,139 @@ haveScript(){
 	node -p '(require("./package.json").scripts || {})["'"$1"'"] || ""' | grep -q .
 }
 
-# Retrieve the ID of the latest beta release
-getBetaID(){
-	printf >&2 'Getting tag of latest beta release... '
-	set -- "`curl -s https://api.github.com/repos/atom/atom/releases \
-	| grep -ioE '"tag_name"\s*:\s*"[^\"]+-beta[0-9]*"' \
-	| head -n1 \
-	| cut -f2 -d: \
-	| tr -d '\"'`"
-	printf >&2 '\e[4m%s\e[0m\n' "$1"
-	
-	printf >&2 'Getting release ID... '
-	set -- "`curl -s https://api.github.com/repos/atom/atom/releases/tags/$1 \
-	| grep -m1 -oE '"id"\s*:\s*[0-9]+' \
-	| head -n1 \
-	| cut -f2 -d:`"
-	printf >&2 '\e[4m%s\e[0m\n' "$1"
-	printf %s "$1"
+# Load JSON data using curl(1)
+loadJSON(){
+	printf >&2 '\e[32m$ \e[1mcurl\e[22m -#fq \e[4m%s\e[0m\n' "$1"
+	curl -#fq "$1"
 }
 
 # Retrieve the ID of the latest stable release
-getStableID(){
-	printf >&2 'Getting ID of latest stable release... '
-	set -- "`curl -s https://api.github.com/repos/atom/atom/releases/latest \
+# - Arguments: [user/repo]
+latestStableID(){
+	printf >&2 'Getting ID of latest stable release\n'
+	{ loadJSON "https://api.github.com/repos/$1/releases/latest" \
 	| grep -oE '"id"\s*:\s*[0-9]+' \
 	| head -n1 \
-	| cut -f2 -d:`"
-	printf >&2 '\e[4m%s\e[0m\n' "$1"
-	printf %s "$1"
+	| cut -f2 -d:; } \
+	|| die 'Failed to load release ID' $?
+}
+
+# Retrieve the tag-name of the latest stable release
+# - Arguments: [user/repo]
+latestStableTag(){
+	printf >&2 'Getting tag-name of latest stable release\n'
+	{ loadJSON "https://api.github.com/repos/$1/releases/latest" \
+	| grep -ioE '"tag_name"\s*:\s*"[^\"]+"' \
+	| head -n1 \
+	| cut -f2 -d: \
+	| tr -d '"'; } \
+	|| die 'Failed to load tag-name' $?
+}
+
+# Retrieve tag-name of latest beta release
+# - Arguments: [user/repo]
+latestBetaTag(){
+	printf >&2 'Getting tag-name of latest beta release\n'
+	{ loadJSON "https://api.github.com/repos/$1/releases" \
+	| grep -ioE '"tag_name"\s*:\s*"[^\"]+-beta[0-9]*"' \
+	| head -n1 \
+	| cut -f2 -d: \
+	| tr -d '"'; } \
+	|| die 'Failed to load tag-name' $?
+}
+
+# Retrieve the ID of the latest beta release
+# - Arguments: [user/repo]
+latestBetaID(){
+	getIDForTag "$1" "`latestStableTag "$1"`"
+}
+
+# Retrieve the ID of a tagged release
+# - Arguments: [user/repo] [tag-name]
+getIDForTag(){
+	printf >&2 'Getting release ID for tag "%s"\n' "$2"
+	{ loadJSON "https://api.github.com/repos/$1/releases/tags/$2" \
+	| grep -m1 -oE '"id"\s*:\s*[0-9]+' \
+	| head -n1 \
+	| cut -f2 -d:; } \
+	|| die 'Failed to load release ID' $?
 }
 
 # Retrieve the download URL of a release asset
-# - getAssetURL [asset-filename] [release-id]
+# - Arguments: [user/repo] [release-id] [asset-filename]
 getAssetURL(){
-	printf >&2 'Getting URL of asset "%s"... ' "$1"
-	set -- "`curl -s https://api.github.com/repos/atom/atom/releases/$2/assets \
-	| grep -oE '"browser_download_url"\s*:\s*"[^\"]+/'"$1"'"' \
+	printf >&2 'Getting URL for asset "%s"\n' "$3"
+	{ loadJSON "https://api.github.com/repos/$1/releases/$2/assets" \
+	| grep -oE '"browser_download_url"\s*:\s*"[^\"]+/'"$3"'"' \
 	| cut -d: -f2,3 \
-	| tr -d '\"'`"
-	case $1 in
-		http*)
-			printf >&2 '\e[4m%s\e[0m\n' "$1"
-			printf %s "$1"
-		;;
-		*)
-			printf >&2 '\e[1m%s\e[0m\n' "Not found"
-			return 1
-		;;
-	esac
+	| tr -d '"' \
+ 	| grep '^http'; } \
+	|| die 'Unable to load asset URL' $?
 }
 
 # Download an asset file
-# - download [save-as] [from-url]
+# - Arguments: [source-url] [target-file]
 downloadAsset(){
-	printf >&2 'Downloading "%s" from \e[4m%s\e[0m\n' "$1" "$2"
-	curl -# -LH 'Accept: application/octet-stream' -o "$1" "$2" || {
-		set -- "$1" "$2" "$?"
-		printf >&2 'Failed to download \e[4m%s\e[0m from \e[4m%s\e[0m\n' "$1" "$2"
-		exit $3
-	}
+	printf >&2 'Downloading "%s" from \e[4m%s\e[0m\n' "$2" "$1"
+	cmd curl -#fqL -H 'Accept: application/octet-stream' -o "$2" "$1" \
+	|| die 'Failed to download file' $?
 }
 
-if [ "$ATOM_CHANNEL" = beta ]; then
-	title 'Installing Atom (Latest beta release)'
-	set -- "`getBetaID`"
-else
-	title 'Installing Atom (Latest stable release)'
-	set -- "`getStableID`"
-fi
+# Download the latest Atom release as a ZIP or Debian package
+# - Arguments: [release-type] [saved-filename]
+# - Example:   `downloadAtom beta atom-mac.zip`
+downloadAtom(){
+	case $1 in
+		beta)   set -- "`latestBetaID atom/atom`"   "$2" ;;
+		stable) set -- "`latestStableID atom/atom`" "$2" ;;
+		*)      die "Unsupported release type: $1"       ;;
+	esac
+	downloadAsset "`getAssetURL atom/atom "$1" "$2"`" "$2" \
+	|| die 'Failed to download Atom' $?
+}
+
+# Verify that the requested channel is valid
+ATOM_CHANNEL=${ATOM_CHANNEL:=stable}
+case $ATOM_CHANNEL in
+	beta)   title 'Installing Atom (Latest beta release)'   ;;
+	stable) title 'Installing Atom (Latest stable release)' ;;
+	*)      die   'Unsupported channel: '"$ATOM_CHANNEL"'"' ;;
+esac
 
 case `uname -s | tr A-Z a-z` in
 	# macOS
 	darwin)
-		set -- atom-mac.zip "$1" .atom-ci
+		set -- atom-mac.zip .atom-ci
 		[ -f "$1" ] && cmd rm -rf "$1"
-		[ -d "$3" ] && cmd rm -rf "$3"
-		downloadAsset $1 "`getAssetURL $1 "$2"`"
+		[ -d "$2" ] && cmd rm -rf "$2"
+		downloadAtom "$ATOM_CHANNEL" "$1"
 		cmd mkdir .atom-ci
-		cmd unzip -q $1 -d $3
+		cmd unzip -q "$1" -d "$2"
 		
 		if [ "$ATOM_CHANNEL" = beta ]; then
 			ATOM_APP_NAME='Atom Beta.app'
 			ATOM_SCRIPT_NAME='atom-beta'
 			ATOM_SCRIPT_PATH='./atom-beta'
-			cmd ln -s "./$3/${ATOM_APP_NAME}/Contents/Resources/app/atom.sh" "${ATOM_SCRIPT_PATH}"
+			cmd ln -s "./$2/${ATOM_APP_NAME}/Contents/Resources/app/atom.sh" "${ATOM_SCRIPT_PATH}"
 		else
 			ATOM_APP_NAME='Atom.app'
 			ATOM_SCRIPT_NAME='atom.sh'
-			ATOM_SCRIPT_PATH="./$3/${ATOM_APP_NAME}/Contents/Resources/app/atom.sh"
+			ATOM_SCRIPT_PATH="./$2/${ATOM_APP_NAME}/Contents/Resources/app/atom.sh"
 		fi
 		
-		ATOM_PATH="./$3"
-		APM_SCRIPT_PATH="./$3/${ATOM_APP_NAME}/Contents/Resources/app/apm/node_modules/.bin/apm"
-		NPM_SCRIPT_PATH="./$3/${ATOM_APP_NAME}/Contents/Resources/app/apm/node_modules/.bin/npm"
-		PATH="${PATH}:${TRAVIS_BUILD_DIR}/$3/${ATOM_APP_NAME}/Contents/Resources/app/apm/node_modules/.bin"
+		ATOM_PATH="./$2"
+		APM_SCRIPT_PATH="./$2/${ATOM_APP_NAME}/Contents/Resources/app/apm/node_modules/.bin/apm"
+		NPM_SCRIPT_PATH="./$2/${ATOM_APP_NAME}/Contents/Resources/app/apm/node_modules/.bin/npm"
+		PATH="${PATH}:${TRAVIS_BUILD_DIR}/$2/${ATOM_APP_NAME}/Contents/Resources/app/apm/node_modules/.bin"
 		export APM_SCRIPT_PATH ATOM_APP_NAME ATOM_PATH ATOM_SCRIPT_NAME ATOM_SCRIPT_PATH NPM_SCRIPT_PATH PATH
 	;;
 	
 	# Linux (Debian assumed)
 	linux)
-		set -- atom-amd64.deb "$1" .atom-ci
+		set -- atom-amd64.deb .atom-ci
 		[ -f "$1" ] && cmd rm -rf "$1"
-		[ -d "$3" ] && cmd rm -rf "$3"
-		downloadAsset $1 "`getAssetURL $1 "$2"`"
+		[ -d "$2" ] && cmd rm -rf "$2"
+		downloadAtom "$ATOM_CHANNEL" "$1"
 		
 		cmd /sbin/start-stop-daemon \
 			--start \
@@ -135,7 +172,7 @@ case `uname -s | tr A-Z a-z` in
 			--exec /usr/bin/Xvfb \
 			-- :99 -ac -screen 0 1280x1024x16
 		DISPLAY=:99; export DISPLAY
-		dpkg-deb -x $1 "${HOME}/$3"
+		dpkg-deb -x "$1" "${HOME}/$2"
 		
 		if [ "$ATOM_CHANNEL" = beta ]; then
 			ATOM_SCRIPT_NAME='atom-beta'
@@ -144,10 +181,10 @@ case `uname -s | tr A-Z a-z` in
 			ATOM_SCRIPT_NAME='atom'
 			APM_SCRIPT_NAME='apm'
 		fi
-		ATOM_SCRIPT_PATH="${HOME}/$3/usr/bin/${ATOM_SCRIPT_NAME}"
-		APM_SCRIPT_PATH="${HOME}/$3/usr/bin/${APM_SCRIPT_NAME}"
-		NPM_SCRIPT_PATH="${HOME}/$3/usr/share/${ATOM_SCRIPT_NAME}/resources/app/apm/node_modules/.bin/npm"
-		PATH="${PATH}:${HOME}/$3/usr/bin"
+		ATOM_SCRIPT_PATH="${HOME}/$2/usr/bin/${ATOM_SCRIPT_NAME}"
+		APM_SCRIPT_PATH="${HOME}/$2/usr/bin/${APM_SCRIPT_NAME}"
+		NPM_SCRIPT_PATH="${HOME}/$2/usr/share/${ATOM_SCRIPT_NAME}/resources/app/apm/node_modules/.bin/npm"
+		PATH="${PATH}:${HOME}/$2/usr/bin"
 		export APM_SCRIPT_NAME APM_SCRIPT_PATH ATOM_SCRIPT_NAME ATOM_SCRIPT_PATH NPM_SCRIPT_PATH PATH
 	;;
 esac
--- src/0-shared.sh
+++ src/0-shared.sh
@@ -2,14 +2,21 @@
 
 # Print a colourful "==> $1"
 title(){
-	printf >&2 '\e[32m==>\e[0m \e[1m%s\e[0m\n' "$1"
+	printf >&2 '\e[34m==>\e[0m \e[1m%s\e[0m\n' "$1"
 }
 
 # Print a command before executing it
 cmd(){
-	printf >&2 '\e[38;5;28m$\e[0m \e[32m%s\e[0m\n' "$*"
+	printf >&2 '\e[32m$ %s\e[0m\n' "$*"
 	if [ "$ATOM_CI_DRY_RUN" ]; then return 0; fi # DEBUG
 	"$@"
+}
+
+# Terminate execution with an error message
+die(){
+	printf >&2 '\e[1;31mfatal:\e[22m %s\e[0m\n' "$1"
+	shift
+	exit ${1:-1}
 }
 
 # Check if a devDependency is listed in package.json
@@ -22,64 +29,95 @@ haveScript(){
 	node -p '(require("./package.json").scripts || {})["'"$1"'"] || ""' | grep -q .
 }
 
-# Retrieve the ID of the latest beta release
-getBetaID(){
-	printf >&2 'Getting tag of latest beta release... '
-	set -- "`curl -s https://api.github.com/repos/atom/atom/releases \
+# Load JSON data using curl(1)
+loadJSON(){
+	printf >&2 '\e[32m$ \e[1mcurl\e[22m -#fq \e[4m%s\e[0m\n' "$1"
+	curl -#fq "$1"
+}
+
+
+# Retrieve the ID of the latest stable release
+# - Arguments: [user/repo]
+latestStableID(){
+	printf >&2 'Getting ID of latest stable release\n'
+	{ loadJSON "https://api.github.com/repos/$1/releases/latest" \
+	| grep -oE '"id"\s*:\s*[0-9]+' \
+	| head -n1 \
+	| cut -f2 -d:; } \
+	|| die 'Failed to load release ID' $?
+}
+
+# Retrieve the tag-name of the latest stable release
+# - Arguments: [user/repo]
+latestStableTag(){
+	printf >&2 'Getting tag-name of latest stable release\n'
+	{ loadJSON "https://api.github.com/repos/$1/releases/latest" \
+	| grep -ioE '"tag_name"\s*:\s*"[^\"]+"' \
+	| head -n1 \
+	| cut -f2 -d: \
+	| tr -d '"'; } \
+	|| die 'Failed to load tag-name' $?
+}
+
+# Retrieve tag-name of latest beta release
+# - Arguments: [user/repo]
+latestBetaTag(){
+	printf >&2 'Getting tag-name of latest beta release\n'
+	{ loadJSON "https://api.github.com/repos/$1/releases" \
 	| grep -ioE '"tag_name"\s*:\s*"[^\"]+-beta[0-9]*"' \
 	| head -n1 \
 	| cut -f2 -d: \
-	| tr -d '\"'`"
-	printf >&2 '\e[4m%s\e[0m\n' "$1"
-	
-	printf >&2 'Getting release ID... '
-	set -- "`curl -s https://api.github.com/repos/atom/atom/releases/tags/$1 \
-	| grep -m1 -oE '"id"\s*:\s*[0-9]+' \
-	| head -n1 \
-	| cut -f2 -d:`"
-	printf >&2 '\e[4m%s\e[0m\n' "$1"
-	printf %s "$1"
+	| tr -d '"'; } \
+	|| die 'Failed to load tag-name' $?
 }
 
-# Retrieve the ID of the latest stable release
-getStableID(){
-	printf >&2 'Getting ID of latest stable release... '
-	set -- "`curl -s https://api.github.com/repos/atom/atom/releases/latest \
-	| grep -oE '"id"\s*:\s*[0-9]+' \
+# Retrieve the ID of the latest beta release
+# - Arguments: [user/repo]
+latestBetaID(){
+	getIDForTag "$1" "`latestStableTag "$1"`"
+}
+
+# Retrieve the ID of a tagged release
+# - Arguments: [user/repo] [tag-name]
+getIDForTag(){
+	printf >&2 'Getting release ID for tag "%s"\n' "$2"
+	{ loadJSON "https://api.github.com/repos/$1/releases/tags/$2" \
+	| grep -m1 -oE '"id"\s*:\s*[0-9]+' \
 	| head -n1 \
-	| cut -f2 -d:`"
-	printf >&2 '\e[4m%s\e[0m\n' "$1"
-	printf %s "$1"
+	| cut -f2 -d:; } \
+	|| die 'Failed to load release ID' $?
 }
 
 # Retrieve the download URL of a release asset
-# - getAssetURL [asset-filename] [release-id]
+# - Arguments: [user/repo] [release-id] [asset-filename]
 getAssetURL(){
-	printf >&2 'Getting URL of asset "%s"... ' "$1"
-	set -- "`curl -s https://api.github.com/repos/atom/atom/releases/$2/assets \
-	| grep -oE '"browser_download_url"\s*:\s*"[^\"]+/'"$1"'"' \
+	printf >&2 'Getting URL for asset "%s"\n' "$3"
+	{ loadJSON "https://api.github.com/repos/$1/releases/$2/assets" \
+	| grep -oE '"browser_download_url"\s*:\s*"[^\"]+/'"$3"'"' \
 	| cut -d: -f2,3 \
-	| tr -d '\"'`"
-	case $1 in
-		http*)
-			printf >&2 '\e[4m%s\e[0m\n' "$1"
-			printf %s "$1"
-		;;
-		*)
-			printf >&2 '\e[1m%s\e[0m\n' "Not found"
-			return 1
-		;;
-	esac
+	| tr -d '"' \
+ 	| grep '^http'; } \
+	|| die 'Unable to load asset URL' $?
 }
 
 # Download an asset file
-# - download [save-as] [from-url]
+# - Arguments: [source-url] [target-file]
 downloadAsset(){
-	printf >&2 'Downloading "%s" from \e[4m%s\e[0m\n' "$1" "$2"
+	printf >&2 'Downloading "%s" from \e[4m%s\e[0m\n' "$2" "$1"
 	if [ "$ATOM_CI_DRY_RUN" ]; then return 0; fi # DEBUG
-	curl -# -LH 'Accept: application/octet-stream' -o "$1" "$2" || {
-		set -- "$1" "$2" "$?"
-		printf >&2 'Failed to download \e[4m%s\e[0m from \e[4m%s\e[0m\n' "$1" "$2"
-		exit $3
-	}
+	cmd curl -#fqL -H 'Accept: application/octet-stream' -o "$2" "$1" \
+	|| die 'Failed to download file' $?
+}
+
+# Download the latest Atom release as a ZIP or Debian package
+# - Arguments: [release-type] [saved-filename]
+# - Example:   `downloadAtom beta atom-mac.zip`
+downloadAtom(){
+	case $1 in
+		beta)   set -- "`latestBetaID atom/atom`"   "$2" ;;
+		stable) set -- "`latestStableID atom/atom`" "$2" ;;
+		*)      die "Unsupported release type: $1"       ;;
+	esac
+	downloadAsset "`getAssetURL atom/atom "$1" "$2"`" "$2" \
+	|| die 'Failed to download Atom' $?
 }
