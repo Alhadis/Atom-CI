Alhadis/Atom-CI

commit 2b47c3933f6be807077cdde00657d62158e2ef72

Force `::endgroup::` to be emitted on a new line
GitHub Actions will apparently only interpret `::` as the beginning of a
command when it's echoed to STDOUT at the beginning of a separate line.

Source: https://github.com/actions/toolkit/blob/dd04665/docs/commands.md

dist/main.sh
@@ -124,7 +124,7 @@ endFold(){
 			foldStack=${foldStack%:$2}
 
 			# FIXME: Same issue/limitation as `startFold()`
-			case $foldStack in *:*) ;; *) printf '::endgroup::\n' ;; esac
+			case $foldStack in *:*) ;; *) printf '\n::endgroup::\n' ;; esac
 			[ ! "$1" = "$2" ] || break
 		done
 	fi

src/0-shared.sh
@@ -126,7 +126,7 @@ endFold(){
 			foldStack=${foldStack%:$2}
 
 			# FIXME: Same issue/limitation as `startFold()`
-			case $foldStack in *:*) ;; *) printf '::endgroup::\n' ;; esac
+			case $foldStack in *:*) ;; *) printf '\n::endgroup::\n' ;; esac
 			[ ! "$1" = "$2" ] || break
 		done
 	fi

commit 48124ffe79132ca12cd6dd9606563af3403dc284

Force ANSI escape to precede newline in APM output
This causes problems when immediately followed by an `::endgroup::` line
(GitHub Actions expects commands to appear immediately after a newline).
A kludgey attempt to solve this was attempted by 2b47c39, which caused a
blank line to appear at the end of every other group. It's been reverted
in favour of a more effective solution.

In addition, don't print error messages to STDERR since it's interleaved
with other messages, making it harder to locate the source of the error.

dist/main.sh
@@ -113,7 +113,7 @@ endFold(){
 		if [ $# -gt 0 ]; then
 			case $foldStack in
 				"$1"|"$1:"*|*":$1"|*":$1:"*) ;;
-				*) printf >&2 'No such fold: %s\n' "$1"; return ;;
+				*) printf 'No such fold: %s\n' "$1"; return ;;
 			esac
 
 		# If no name was passed, default to whatever fold was most recently opened
@@ -124,7 +124,7 @@ endFold(){
 			foldStack=${foldStack%:$2}
 
 			# FIXME: Same issue/limitation as `startFold()`
-			case $foldStack in *:*) ;; *) printf '\n::endgroup::\n' ;; esac
+			case $foldStack in *:*) ;; *) printf '::endgroup::\n' ;; esac
 			[ ! "$1" = "$2" ] || break
 		done
 	fi
@@ -369,14 +369,14 @@ showVersions(){
 apmInstall(){
 	endFold 'installers'
 	startFold 'install-deps' 'Installing dependencies'
-	set -- "$1" "`sgr 4`" "`sgr 24`"
+	set -- "$1" "`sgr 4`" "`sgr 24`" '/✓$/{N;s/\n//;s/$/\n/;}'
 	if [ -f package-lock.json ] && apmHasCI; then
 		printf 'Installing from %s%s%s\n' "$2" package-lock.json "$3"
-		cmd "${APM_SCRIPT_PATH}" ci $1
+		cmd "${APM_SCRIPT_PATH}" ci $1 | sed "$4"
 	else
 		printf 'Installing from %s%s%s\n' "$2" package.json "$3"
 		cmd "${APM_SCRIPT_PATH}" install $1
-		cmd "${APM_SCRIPT_PATH}" clean
+		cmd "${APM_SCRIPT_PATH}" clean | sed "$4"
 	fi
 }
 

src/0-shared.sh
@@ -115,7 +115,7 @@ endFold(){
 		if [ $# -gt 0 ]; then
 			case $foldStack in
 				"$1"|"$1:"*|*":$1"|*":$1:"*) ;;
-				*) printf >&2 'No such fold: %s\n' "$1"; return ;;
+				*) printf 'No such fold: %s\n' "$1"; return ;;
 			esac
 
 		# If no name was passed, default to whatever fold was most recently opened
@@ -126,7 +126,7 @@ endFold(){
 			foldStack=${foldStack%:$2}
 
 			# FIXME: Same issue/limitation as `startFold()`
-			case $foldStack in *:*) ;; *) printf '\n::endgroup::\n' ;; esac
+			case $foldStack in *:*) ;; *) printf '::endgroup::\n' ;; esac
 			[ ! "$1" = "$2" ] || break
 		done
 	fi

src/2-install-deps.sh
@@ -27,14 +27,14 @@ showVersions(){
 apmInstall(){
 	endFold 'installers'
 	startFold 'install-deps' 'Installing dependencies'
-	set -- "$1" "`sgr 4`" "`sgr 24`"
+	set -- "$1" "`sgr 4`" "`sgr 24`" '/✓$/{N;s/\n//;s/$/\n/;}'
 	if [ -f package-lock.json ] && apmHasCI; then
 		printf 'Installing from %s%s%s\n' "$2" package-lock.json "$3"
-		cmd "${APM_SCRIPT_PATH}" ci $1
+		cmd "${APM_SCRIPT_PATH}" ci $1 | sed "$4"
 	else
 		printf 'Installing from %s%s%s\n' "$2" package.json "$3"
 		cmd "${APM_SCRIPT_PATH}" install $1
-		cmd "${APM_SCRIPT_PATH}" clean
+		cmd "${APM_SCRIPT_PATH}" clean | sed "$4"
 	fi
 }
 