a3b26cc362e6d7673a679b05529dcc3f55f287c0
Alhadis <gardnerjohng@gmail.com>
2021-07-19 12:23:37 +1000

    Add logic for linting files and running tests

---
+++ src/3-run-tasks.ps1
@@ -0,0 +1,44 @@
+#!/usr/bin/env pwsh
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+Import-Module -Name (Join-Path $PSScriptRoot "0-shared.psm1")
+$VerbosePreference = "Continue"
+
+assertValidProject
+setupEnvironment
+
+title "Running tasks"
+
+# Run "lint" script if one exists in `package.json`
+if(haveScript "lint"){
+	cmd "$env:NPM_SCRIPT_PATH" run lint
+}
+# If not, use assumed defaults
+else{
+	foreach($linter in "coffeelint", "eslint", "tslint"){
+		if(-not (haveDep $linter)) { continue }
+		Write-Host "Linting package with $linter..."
+		for($dir -in "lib", "src", "spec", "test"){
+			if(isDir $dir){
+				cmd "npx" $linter "./$dir"
+			}
+		}
+	}
+}
+
+
+# Run the `package.json` "test" script if one exists
+if(haveScript "test"){
+	cmd "$env:NPM_SCRIPT_PATH" run test
+}
+# If not, locate test-suite manually
+else{
+	foreach($dir in "spec", "specs", "test", "tests"){
+		if(isDir $dir){
+			Write-Host "Running specs..."
+			cmd "$env:ATOM_SCRIPT_PATH" --test "./$dir"
+			break
+		}
+	}
+}
